77652e9b2d62067bd8799ef3c575e9711a62935c
Maya Jerndahl <144351614+MjBonk@users.noreply.github.com>
2024-01-28 20:19:01 +0100

    test gif fot netlify

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,10 +4,8 @@ function Title() {
 		<div className=" text-3xl md:text-4xl lg:text-5xl font-monserrat p-2 text-title flex flex-col items-center text-center md:justify-center md:flex-row">
 			<h1>I'LL PUT YOU IN THE </h1>
 			<div className=" flex items-center">
-				<img
-					src="./public/Comp 1.gif"
-					className=" w-36 md:w-40 lg:w-52 h-[100%] -my-5 -mx-1"
-				></img>
+				<img src="./public/Comp 1.gif" className=" w-36 md:w-40 lg:w-52 h-[100%] -my-5 -mx-1" />
+				<img src="./public/giphy.gif" alt="" />
 				<h1>TEAM</h1>
 			</div>
 		</div>
